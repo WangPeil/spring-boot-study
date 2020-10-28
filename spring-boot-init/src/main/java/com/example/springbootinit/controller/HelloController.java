9505bca594828e5a590ba8fed7de138e3a80153f
peili.wang <w_peil@163.com>
2020-10-28 22:40:44 +0800

    spring-init update

--- spring-boot-init/src/main/java/com/example/springbootinit/controller/HelloController.java
+++ spring-boot-init/src/main/java/com/example/springbootinit/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.springbootinit.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello, spring boot";
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable String name) {
+        return String.format("hello %s, spring boot", name);
     }
 }
